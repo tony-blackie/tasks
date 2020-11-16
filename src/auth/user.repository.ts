52d244aaedc514c7d5f6f899da1815b490bfc92a
Tony Spaskyi <jim.casey321@gmail.com>
2020-11-16 22:52:13 +0200

    feat: add conflict error when trying to create new user with non-unique username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,6 +11,16 @@ export class UserRepository extends Repository<User> {
         const user = new User();
         user.username = username;
         user.password = password;
-        await user.save();
+
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Username already exists');
+            }
+
+            throw new InternalServerErrorException();
+        }
+        
     }
 }
